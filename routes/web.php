1c2d79b877f6df1ac152d55d4b4b245c97e47345
sotobayashi kaoru <sotobayashi@2ndwave.jp>
2019-08-07 11:37:22 +0900

    2019/08/07(2)

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('posts','PostsController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
